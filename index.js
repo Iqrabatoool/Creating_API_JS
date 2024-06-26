f1495ae0707c41df4ee4e9c526f614eb32312947
Iqra Batool <157022596+Iqrabatoool@users.noreply.github.com>
2024-06-27 00:26:09 +0500

    Updated index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const options = {
+    dotfiles: 'ignore',
+    //Entity Tag
+    etag: false,
+    extensions: ['htm', 'html'],
+    //cache control
+    index: false,
+    //Maximum Age for caching Static files (1d = 1 day)
+    maxAge: '1d',
+    redirect: false,
+    setHeaders (res, path, stat) {
+      res.set('x-timestamp', Date.now())
+    }
+  }
+  
+  app.use(express.static('public', options))
+
+  //creating Router_options
+  const router = express.Router([options])
