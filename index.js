65ce6b3750976c04d3313d0a4f7df6af6491612b
Iqra Batool <157022596+Iqrabatoool@users.noreply.github.com>
2024-06-27 02:28:53 +0500

    Making Code simplier and Effective

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const options = {
-    dotfiles: 'ignore',
-    //Entity Tag
-    etag: false,
-    extensions: ['htm', 'html'],
-    //cache control
-    index: false,
-    //Maximum Age for caching Static files (1d = 1 day)
-    maxAge: '1d',
-    redirect: false,
-    setHeaders (res, path, stat) {
-      res.set('x-timestamp', Date.now())
-    }
-  }
-  
-  app.use(express.static('public', options))
-  
+//Import Express
+const app = require('express')();
+
+//Port
+const PORT = 8080;
+
+app.listen(
+  PORT,
+  () => console.log(`Server running on port http://localhost:${PORT}`)
+)
+
+app.get('/ayo', (req , res)=>{
+  res.status(200).send({
+    name : 'ayo',
+    message : 'Yuck'
+  })
+})
